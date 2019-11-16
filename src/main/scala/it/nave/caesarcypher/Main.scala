06f37ac2092db27dd39c352039fe2cba31425350
Claudio Nave <claudionave97@gmail.com>
2019-11-16 11:15:41 +0100

    Creato attore per rappresentare i singoli caratteri

--- src/main/scala/it/nave/caesarcypher/Main.scala
+++ src/main/scala/it/nave/caesarcypher/Main.scala
@@ -19,6 +19,53 @@
 
 package it.nave.caesarcypher
 
+import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.typed.{ActorRef, Behavior}
+
+import scala.io.StdIn
+
 object Main extends App {
-  println("Hello, World!")
+  print("Inserisci una stringa da criptare: ")
+  StdIn
+    .readLine()
+    .view
+    .zipWithIndex
+    .foreach(println(_))
+}
+
+object Character {
+
+  sealed trait Message {
+    val shift: Int
+    val index: Int
+
+    abstract def decrement(): Message
+  }
+
+  final case class Crypt(shift: Int, index: Int) extends Message {
+    override def decrement() = Crypt(shift - 1, index)
+  }
+
+  final case class Decrypt(shift: Int, index: Int) extends Message {
+    override def decrement() = Decrypt(shift - 1, index)
+  }
+
+  def apply(char: Char, nextChar: ActorRef[Message], previousChar: ActorRef[Message]): Behavior[Message] = {
+    Behaviors.receive { (context, message) =>
+      message match {
+        case crypt: Crypt =>
+          context.log.info("Actor of char {}: received crypt message {}", char, crypt)
+          handleMessage(crypt, nextChar)
+        case decrypt: Decrypt =>
+          context.log.info("Actor of char {}: received decrypt message {}", char, decrypt)
+          handleMessage(decrypt, previousChar)
+      }
+      Behaviors.same
+    }
+  }
+
+  private def handleMessage(message: Message, replyTo: ActorRef[Message]): Unit = {
+    if (message.shift > 0) replyTo ! message.decrement else println("TODO Implementare raccolta e stampa dei caratteri finali") // TODO
+  }
+
 }
